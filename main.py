2f196d1b2b76c770e5294ead00357488a589e790
Aryog <yogesh000aryal@gmail.com>
2024-05-15 21:56:24 +0545

    data ingestion completed

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from kidneyClassifier import logger
+from kidneyClassifier.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-
-logger.info("Welcome to kidney classifier!");
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f'>>>>> Stage {STAGE_NAME} started <<<<<')
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>> Stage {STAGE_NAME} completed <<<<<\n\n x==========x')
+except Exception as e:
+    logger.exception(e)
+    raise e

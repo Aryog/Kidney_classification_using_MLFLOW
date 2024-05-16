9f27f0c4f81a0abd3f02dcc272b5aea14c868706
Aryog <yogesh000aryal@gmail.com>
2024-05-16 23:13:51 +0545

    prepare data model

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from kidneyClassifier import logger
 from kidneyClassifier.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
+from kidneyClassifier.pipeline.Stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f'>>>>> Stage {STAGE_NAME} started <<<<<')
     obj = DataIngestionTrainingPipeline()
     obj.main()
+    logger.info(f'>>>>> Stage {STAGE_NAME} completed <<<<<\n\n x==========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Base Model Preparation Stage"
+try:
+    logger.info(f'******************')
+    logger.info(f'>>>>> Stage {STAGE_NAME} started <<<<<')
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f'>>>>> Stage {STAGE_NAME} completed <<<<<\n\n x==========x')
 except Exception as e:
     logger.exception(e)
